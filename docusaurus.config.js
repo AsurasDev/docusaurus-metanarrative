15e586c8fdbab9eb19e8ef4f3b22ea7ce973d81a
AsurasDev <eduar.ramos88@gmail.com>
2025-10-10 19:39:57 -0500

    latex support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,6 +51,8 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+           remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         blog: {
           showReadingTime: true,
